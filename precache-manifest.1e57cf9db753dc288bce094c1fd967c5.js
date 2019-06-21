b225b03940986f477a612c809e1a63668222cbd5
Zack Calaily <zack.calaily@gmail.com>
2019-06-21 01:35:12 -0400

    Updates

---
+++ precache-manifest.1e57cf9db753dc288bce094c1fd967c5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a39419ae3bf37ed4b2b1662a582a492",
+    "url": "/sortable-procedure/index.html"
+  },
+  {
+    "revision": "1ed0302a5377bbf11362",
+    "url": "/sortable-procedure/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "84030b99b9fc9e7f5eb0",
+    "url": "/sortable-procedure/static/css/main.e40bb6c2.chunk.css"
+  },
+  {
+    "revision": "1ed0302a5377bbf11362",
+    "url": "/sortable-procedure/static/js/2.f242d052.chunk.js"
+  },
+  {
+    "revision": "84030b99b9fc9e7f5eb0",
+    "url": "/sortable-procedure/static/js/main.90ee167f.chunk.js"
+  },
+  {
+    "revision": "e933730fde9cbc86a1b5",
+    "url": "/sortable-procedure/static/js/runtime~main.dc7b509e.js"
+  },
+  {
+    "revision": "24932ad03d1891ed270a4ede16bdf03e",
+    "url": "/sortable-procedure/static/media/avenir-medium-normal.24932ad0.woff2"
+  },
+  {
+    "revision": "6b04d1656ab054b0e3feea2c2eb0355e",
+    "url": "/sortable-procedure/static/media/avenir-medium-normal.6b04d165.woff"
+  },
+  {
+    "revision": "457e94a5b665614917d1dc45a13d97b8",
+    "url": "/sortable-procedure/static/media/avenir-regular-normal.457e94a5.woff2"
+  },
+  {
+    "revision": "a17c827d23b2077f3be3824965774bba",
+    "url": "/sortable-procedure/static/media/avenir-regular-normal.a17c827d.woff"
+  },
+  {
+    "revision": "06dcc64a7250e99cb84ee591399b3c65",
+    "url": "/sortable-procedure/static/media/dotted-border.06dcc64a.svg"
+  }
+]);
